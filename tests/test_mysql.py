14f17c76224078ee5be91e1fe9b4a9a81eca30c8
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-10-15 21:10:15 +0200

    Unit and integration tests

---
+++ tests/test_mysql.py
@@ -0,0 +1,59 @@
+import pytest
+
+from sdRDM import DataModel
+from sdrdm_database import create_tables
+
+
+@pytest.mark.integration
+def test_mysql():
+    from sdrdm_database import DBConnector
+
+    # Establish a connection to the database
+    db = DBConnector(
+        username="root",
+        password="root",
+        host="localhost",
+        db_name="db",
+        port=3306,
+        dbtype="mysql",
+    )
+
+    assert db.connection is not None, "Connection not established"
+
+    # Load model
+    lib = DataModel.from_markdown("./.github/integration/model.md")
+    create_tables(db_connector=db, model=lib.Test)
+
+    # Check tables
+    expected_tables = set(["Test", "nested"])
+    assert (
+        set(db.connection.list_tables()) == expected_tables
+    ), f"Expected tables '{expected_tables}' but got '{db.connection.list_tables()}'"
+
+    # Check schema
+    table = db.connection.table("Test")
+    schema = {
+        key: ("!" if not dtype.nullable else "") + dtype.__class__.__name__.lower()
+        for key, dtype in table.schema().fields.items()
+    }
+
+    expected = {
+        "id": "!int32",
+        "name": "string",
+        "int_value": "int64",
+        "float_value": "float64",
+        "bool_value": "boolean",
+    }
+
+    assert schema == expected, f"Expected schema '{expected}' but got '{schema}'"
+
+    # Add data
+    obj = lib.Test(
+        id=0, int_value=1, float_value=1.0, bool_value=True, name="Hello"
+    ).dict(exclude_unset=True)
+
+    db.connection.insert("Test", [obj])
+
+    expected = {**obj, "id": 1}
+    entry = db.connection.table("Test").execute().loc[0].to_dict()
+    assert entry == expected, f"Expected entry '{expected}' but got '{entry}'"
---
+++ tests/test_tablecreator.py
@@ -0,0 +1,127 @@
+from functools import partial
+from typing import Optional
+from pydantic import BaseModel
+import pytest
+from sdrdm_database.dbconnector import DBConnector
+from sdrdm_database.tablecreator import (
+    _create_table_schema,
+    _map_type,
+    _populate_schema,
+    _handle_foreign_keys,
+)
+
+
+class MockDataModel(BaseModel):
+    foo: str
+    bar: Optional[int] = None
+
+
+def test_map_type():
+    # Test mapping of integer type
+    assert _map_type(int, True) == "!int64", "Wrong mapping of mandatory integer type"
+    assert _map_type(int, False) == "int64", "Wrong mapping of integer type"
+
+    # Test mapping of float type
+    assert _map_type(float, True) == "!float64", "Wrong mapping of mandatory float type"
+    assert _map_type(float, False) == "float64", "Wrong mapping of float type"
+
+    # Test mapping of string type
+    assert _map_type(str, True) == "!string", "Wrong mapping of mandatory string type"
+    assert _map_type(str, False) == "string", "Wrong mapping of string type"
+
+    # Test mapping of boolean type
+    assert _map_type(bool, True) == "!boolean", "Wrong mapping of mandatory bool type"
+    assert _map_type(bool, False) == "boolean", "Wrong mapping of boolean type"
+
+    # Test mapping of unsupported type
+    with pytest.raises(ValueError):
+        _map_type(list, True)
+
+
+def test_populate_schema():
+    class MockDataModel(BaseModel):
+        foo: str
+        bar: Optional[int] = None
+
+    schema = {}
+
+    foo = MockDataModel.__fields__["foo"]
+    bar = MockDataModel.__fields__["bar"]
+
+    _populate_schema(attr=foo, schema=schema)
+    _populate_schema(attr=bar, schema=schema)
+
+    assert schema == {"foo": "!string", "bar": "int64"}
+
+
+def test_handle_foreign_keys():
+    db_connector = DBConnector(
+        db_name="Test",
+        username="root",
+        password="root",
+        dbtype="mysql",
+        host="localhost",
+        port=3306,
+    )
+
+    parent = "parent"
+    table_name = "table_name"
+    fk_commands = []
+
+    _handle_foreign_keys(
+        parent=parent,
+        table_name=table_name,
+        db_connector=db_connector,
+        fk_commands=fk_commands,
+    )
+
+    expected_kwargs = {
+        "table_name": table_name,
+        "foreign_key": parent,
+        "reference_table": parent,
+        "reference_column": "id",
+        "con": db_connector.connection,
+    }
+
+    assert fk_commands[0].keywords == expected_kwargs
+    assert fk_commands[0].func == db_connector.__commands__.add_foreign_key
+
+
+def test_create_table_schema():
+    db_connector = DBConnector(
+        db_name="Test",
+        username="root",
+        password="root",
+        dbtype="mysql",
+        host="localhost",
+        port=3306,
+    )
+
+    data_model = MockDataModel
+    table_name = "table_name"
+    schemes = []
+
+    result = _create_table_schema(
+        db_connector=db_connector,
+        data_model=data_model,
+        table_name=table_name,
+        schemes=schemes,
+    )
+
+    pk_command = result[0].pop("pk_command")
+    expected_pk = partial(
+        db_connector.__commands__.add_primary_key,
+        table_name=table_name,
+        primary_key="id",
+        con=db_connector.connection,
+    )
+
+    expected_schema = {
+        "name": "table_name",
+        "schema": {"foo": "!string", "bar": "int64"},
+        "fk_commands": [],
+    }
+
+    assert result == [expected_schema]
+    assert pk_command.keywords == expected_pk.keywords
+    assert pk_command.func == expected_pk.func
