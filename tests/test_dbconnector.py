20c4a86a9ac2d8980c2c61a2a0455027e15b6b59
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-10-15 21:10:15 +0200

    Unit and integration tests

---
+++ tests/test_dbconnector.py
@@ -0,0 +1,28 @@
+from sdrdm_database import DBConnector
+from sdrdm_database.commands import PostgresCommands, MySQLCommands
+
+
+def test_commands():
+    # MySQL case
+    db = DBConnector(
+        db_name="Test",
+        username="root",
+        password="root",
+        host="localhost",
+        port=3306,
+        dbtype="mysql",
+    )
+
+    assert db.__commands__ == MySQLCommands, "Wrong commands class"
+
+    # Postgres case
+    db = DBConnector(
+        db_name="Test",
+        username="root",
+        password="root",
+        host="localhost",
+        port=5432,
+        dbtype="postgres",
+    )
+
+    assert db.__commands__ == PostgresCommands, "Wrong commands class"
