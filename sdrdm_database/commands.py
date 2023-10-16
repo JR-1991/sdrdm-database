ca212c3ccdc1a423866e533394ebe92c6b0eeb1f
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-10-16 11:06:21 +0200

    Changed ID to varchar

--- sdrdm_database/commands.py
+++ sdrdm_database/commands.py
@@ -30,7 +30,7 @@ class MySQLCommands(MetaCommands):
     ):
         try:
             con.raw_sql(
-                f"ALTER TABLE {table_name} ADD COLUMN {primary_key} INT AUTO_INCREMENT PRIMARY KEY;"
+                f"ALTER TABLE {table_name} ADD COLUMN {primary_key} VARCHAR(36) PRIMARY KEY;"
             )
         except Exception as e:
             print(f"Could not add primary key {primary_key} for table {table_name}: ")
@@ -46,7 +46,7 @@ class MySQLCommands(MetaCommands):
     ):
         try:
             con.raw_sql(
-                f"ALTER TABLE {table_name} ADD COLUMN {foreign_key} INT NOT NULL;"
+                f"ALTER TABLE {table_name} ADD COLUMN {foreign_key} VARCHAR(36);"
             )
             con.raw_sql(
                 f"ALTER TABLE {table_name} ADD FOREIGN KEY ({foreign_key}) REFERENCES {reference_table}({reference_column});"
@@ -66,7 +66,9 @@ class PostgresCommands(MetaCommands):
         con: "BaseAlchemyBackend",
     ):
         try:
-            con.raw_sql(f"ALTER TABLE {table_name} ADD PRIMARY KEY ({primary_key});")
+            con.raw_sql(
+                f"ALTER TABLE {table_name} ADD COLUMN {primary_key} UUID PRIMARY KEY;"
+            )
         except Exception as e:
             print(f"Could not add primary key {primary_key} for table {table_name}: ")
             raise e
@@ -80,9 +82,7 @@ class PostgresCommands(MetaCommands):
         con: "BaseAlchemyBackend",
     ):
         try:
-            con.raw_sql(
-                f"ALTER TABLE {table_name} ADD COLUMN {foreign_key} INT NOT NULL;"
-            )
+            con.raw_sql(f"ALTER TABLE {table_name} ADD COLUMN {foreign_key} UUID;")
             con.raw_sql(
                 f"ALTER TABLE {table_name} ADD FOREIGN KEY ({foreign_key}) REFERENCES {reference_table}({reference_column});"
             )
