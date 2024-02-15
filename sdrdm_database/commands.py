d43dc651dc4f07bc6b40ac308d3833c3e49544fa
Jan Range <range.jan@web.de>
2024-02-15 11:22:07 +0100

    add join table creation command

--- sdrdm_database/commands.py
+++ sdrdm_database/commands.py
@@ -13,6 +13,7 @@ class MetaCommands(ABC):
 
     @abstractmethod
     def add_foreign_key(
+        self,
         table_name: str,
         foreign_key: str,
         reference_table: str,
@@ -34,6 +35,15 @@ class MetaCommands(ABC):
     @abstractmethod
     def change_all_fields_to_varchar(
         table_name: str,
+        dbconnector: "DBConnector",
+    ):
+        pass
+
+    @abstractmethod
+    def create_join_table(
+        table_name: str,
+        table1: str,
+        table2: str,
         dbconnector: "DBConnector",
     ):
         pass
@@ -74,7 +84,7 @@ class MySQLCommands(MetaCommands):
                 f"Could not add foreign key {foreign_key} for table {table_name} to {reference_table}({reference_column}): "
             )
             raise e
-        
+
     @staticmethod
     def add_foreign_key_constraint(
         table_name: str,
@@ -92,7 +102,7 @@ class MySQLCommands(MetaCommands):
                 f"Could not add foreign key constraint {foreign_key} for table {table_name} to {reference_table}({reference_column}): "
             )
             raise e
-        
+
     @staticmethod
     def change_all_fields_to_varchar(
         table_name: str,
@@ -110,8 +120,27 @@ class MySQLCommands(MetaCommands):
                 f"Could not change column {column} of table {table_name} to varchar(36): "
             )
             raise e
-        
-        
+
+    @staticmethod
+    def create_join_table(
+        table_name: str,
+        table1: str,
+        table2: str,
+        dbconnector: "DBConnector",
+    ):
+        try:
+            dbconnector.connection.raw_sql(
+                f"CREATE TABLE {table_name} ({table1}_id VARCHAR(36), {table2}_id VARCHAR(36));"
+            )
+            dbconnector.connection.raw_sql(
+                f"ALTER TABLE {table_name} ADD FOREIGN KEY ({table1}_id) REFERENCES {table1}(id);"
+            )
+            dbconnector.connection.raw_sql(
+                f"ALTER TABLE {table_name} ADD FOREIGN KEY ({table2}_id) REFERENCES {table2}(id);"
+            )
+        except Exception as e:
+            print(f"Could not create join table {table_name}: ")
+            raise e
 
 
 class PostgresCommands(MetaCommands):
@@ -168,7 +197,7 @@ class PostgresCommands(MetaCommands):
                 f"Could not add foreign key {foreign_key} for table {table_name} to {reference_table}({reference_column}): "
             )
             raise e
-        
+
     @staticmethod
     def add_foreign_key_constraint(
         table_name: str,
@@ -181,10 +210,36 @@ class PostgresCommands(MetaCommands):
             dbconnector.connection.raw_sql(
                 f"ALTER TABLE {table_name} ADD FOREIGN KEY ({foreign_key}) REFERENCES {reference_table}({reference_column});"
             )
-            print("added constraint", table_name, foreign_key, reference_table, reference_column)
+            print(
+                "added constraint",
+                table_name,
+                foreign_key,
+                reference_table,
+                reference_column,
+            )
         except Exception as e:
             print(
                 f"Could not add foreign key constraint {foreign_key} for table {table_name} to {reference_table}({reference_column}): "
             )
             raise e
-        
+
+    @staticmethod
+    def create_join_table(
+        table_name: str,
+        table1: str,
+        table2: str,
+        dbconnector: "DBConnector",
+    ):
+        try:
+            dbconnector.connection.raw_sql(
+                f"CREATE TABLE {table_name} (id VARCHAR(36) PRIMARY KEY, {table1}_id VARCHAR(36), {table2}_id VARCHAR(36));"
+            )
+            dbconnector.connection.raw_sql(
+                f"ALTER TABLE {table_name} ADD FOREIGN KEY ({table1}_id) REFERENCES {table1}(id);"
+            )
+            dbconnector.connection.raw_sql(
+                f"ALTER TABLE {table_name} ADD FOREIGN KEY ({table2}_id) REFERENCES {table2}(id);"
+            )
+        except Exception as e:
+            print(f"Could not create join table {table_name}: ")
+            raise e
