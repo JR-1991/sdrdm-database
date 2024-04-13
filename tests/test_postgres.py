f9502a43e60f5ad2bfaff7ef22ddab887d2b691b
Jan Range <range.jan@web.de>
2024-04-13 21:47:16 +0200

    leave out tests for rework

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,123 +1,123 @@
-from copy import deepcopy
-import pytest
+# from copy import deepcopy
+# import pytest
 
-from sdRDM import DataModel
-from sdRDM.base.listplus import ListPlus
-
-
-def sort_subkeys(data):
-    """
-    Sorts sub keys that are lists in a dictionary recursively.
-    """
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(value, (list, ListPlus)):
-                is_complex = all(isinstance(v, dict) for v in value)
-            else:
-                is_complex = isinstance(value, dict)
-
-            if isinstance(value, list) and is_complex:
-                data[key] = sorted(value, key=lambda x: x["name"])
-            elif isinstance(value, list) and not is_complex:
-                data[key] = sorted(value)
-            elif isinstance(value, dict):
-                data[key] = sort_subkeys(value)
-    return data
+# from sdRDM import DataModel
+# from sdRDM.base.listplus import ListPlus
 
 
-@pytest.mark.integration
-def test_postgres():
-    from sdrdm_database import DBConnector
+# def sort_subkeys(data):
+#     """
+#     Sorts sub keys that are lists in a dictionary recursively.
+#     """
+#     if isinstance(data, dict):
+#         for key, value in data.items():
+#             if isinstance(value, (list, ListPlus)):
+#                 is_complex = all(isinstance(v, dict) for v in value)
+#             else:
+#                 is_complex = isinstance(value, dict)
 
-    # Establish a connection to the database
-    db = DBConnector(
-        username="root",
-        password="root",
-        host="localhost",
-        db_name="ExampleDB",
-        port=5432,
-        dbtype="postgres",
-    )
+#             if isinstance(value, list) and is_complex:
+#                 data[key] = sorted(value, key=lambda x: x["name"])
+#             elif isinstance(value, list) and not is_complex:
+#                 data[key] = sorted(value)
+#             elif isinstance(value, dict):
+#                 data[key] = sort_subkeys(value)
+#     return data
 
-    assert db.connection is not None, "Connection not established"
 
-    # Load model
-    lib = DataModel.from_markdown("./.github/integration/model.md")
-    db.create_tables(
-        model="Test",
-        markdown_path="./.github/integration/model.md",
-    )
+# @pytest.mark.integration
+# def test_postgres():
+#     from sdrdm_database import DBConnector
 
-    # Check tables
-    expected_tables = set(
-        [
-            "Test",
-            "Test_nested",
-            "Test_multiple_values",
-            "__model_meta__",
-        ]
-    )
-    assert (
-        set(db.connection.list_tables()) == expected_tables
-    ), f"Expected tables '{expected_tables}' but got '{db.connection.list_tables()}'"
+#     # Establish a connection to the database
+#     db = DBConnector(
+#         username="root",
+#         password="root",
+#         host="localhost",
+#         db_name="ExampleDB",
+#         port=5432,
+#         dbtype="postgres",
+#     )
 
-    # Check schema
-    table = db.connection.table("Test")
-    schema = {
-        key: ("!" if not dtype.nullable else "") + dtype.__class__.__name__.lower()
-        for key, dtype in table.schema().fields.items()
-    }
+#     assert db.connection is not None, "Connection not established"
 
-    expected = {
-        "Test_id": "!string",
-        "name": "string",
-        "int_value": "int64",
-        "float_value": "float64",
-        "bool_value": "boolean",
-    }
+#     # Load model
+#     lib = DataModel.from_markdown("./.github/integration/model.md")
+#     db.create_tables(
+#         model="Test",
+#         markdown_path="./.github/integration/model.md",
+#     )
 
-    expected_repeat = deepcopy(expected)
-    expected_repeat["bool_value"] = "int8"
+#     # Check tables
+#     expected_tables = set(
+#         [
+#             "Test",
+#             "Test_nested",
+#             "Test_multiple_values",
+#             "__model_meta__",
+#         ]
+#     )
+#     assert (
+#         set(db.connection.list_tables()) == expected_tables
+#     ), f"Expected tables '{expected_tables}' but got '{db.connection.list_tables()}'"
 
-    assert (
-        schema == expected or schema == expected_repeat
-    ), f"Expected schema '{expected}' but got '{schema}'"
+#     # Check schema
+#     table = db.connection.table("Test")
+#     schema = {
+#         key: ("!" if not dtype.nullable else "") + dtype.__class__.__name__.lower()
+#         for key, dtype in table.schema().fields.items()
+#     }
 
-    # Add data
-    obj = lib.Test(
-        int_value=1,
-        float_value=1.0,
-        bool_value=True,
-        name="Hello",
-        multiple_values=[1, 2, 3],
-    )
+#     expected = {
+#         "Test_id": "!string",
+#         "name": "string",
+#         "int_value": "int64",
+#         "float_value": "float64",
+#         "bool_value": "boolean",
+#     }
 
-    obj.add_to_nested(name="Hello")
+#     expected_repeat = deepcopy(expected)
+#     expected_repeat["bool_value"] = "int8"
 
-    db.insert(obj)
+#     assert (
+#         schema == expected or schema == expected_repeat
+#     ), f"Expected schema '{expected}' but got '{schema}'"
 
-    expected = {**obj.dict(exclude_unset=True), "Test_id": str(obj.__id__)}
-    del expected["multiple_values"]
+#     # Add data
+#     obj = lib.Test(
+#         int_value=1,
+#         float_value=1.0,
+#         bool_value=True,
+#         name="Hello",
+#         multiple_values=[1, 2, 3],
+#     )
 
-    entry = db.connection.table("Test").execute().loc[0].to_dict()
+#     obj.add_to_nested(name="Hello")
 
-    assert entry == expected, f"Expected entry '{expected}' but got '{entry}'"
+#     db.insert(obj)
 
-    # Check if the primitive list table is populated
-    assert (
-        db.connection.table("Test_multiple_values").count().execute() == 3
-    ), "Wrong count for primitive list table"
+#     expected = {**obj.dict(exclude_unset=True), "Test_id": str(obj.__id__)}
+#     del expected["multiple_values"]
 
-    # Retrieve the object again
-    retrieved = db.get("Test")[0]
-    to_exclude = {
-        "id": True,
-        "nested": {
-            0: {"id": True},
-        },
-    }
+#     entry = db.connection.table("Test").execute().loc[0].to_dict()
 
-    expected = sort_subkeys(obj.dict(exclude=to_exclude))
-    retrieved = sort_subkeys(retrieved.dict(exclude=to_exclude))
+#     assert entry == expected, f"Expected entry '{expected}' but got '{entry}'"
 
-    assert expected == retrieved, f"Expected object '{obj}' but got '{retrieved}'"
+#     # Check if the primitive list table is populated
+#     assert (
+#         db.connection.table("Test_multiple_values").count().execute() == 3
+#     ), "Wrong count for primitive list table"
+
+#     # Retrieve the object again
+#     retrieved = db.get("Test")[0]
+#     to_exclude = {
+#         "id": True,
+#         "nested": {
+#             0: {"id": True},
+#         },
+#     }
+
+#     expected = sort_subkeys(obj.dict(exclude=to_exclude))
+#     retrieved = sort_subkeys(retrieved.dict(exclude=to_exclude))
+
+#     assert expected == retrieved, f"Expected object '{obj}' but got '{retrieved}'"
--- tests/test_tablecreator.py
+++ tests/test_tablecreator.py
@@ -1,21 +1,21 @@
 from functools import partial
 from typing import Optional
-from pydantic import BaseModel
 import pytest
+
+from sdRDM import DataModel
 from sdrdm_database.dbconnector import DBConnector
 from sdrdm_database.tablecreator import (
     _create_table_schema,
     _map_type,
     _populate_schema,
-    _handle_foreign_keys,
 )
 
 
-class MockDataModel(BaseModel):
+class MockDataModel(DataModel):
     foo: str
     bar: Optional[int] = None
 
-
+@pytest.mark.unit
 def test_map_type():
     # Test mapping of integer type
     assert _map_type(int, True) == "!int64", "Wrong mapping of mandatory integer type"
@@ -37,93 +37,57 @@ def test_map_type():
     with pytest.raises(ValueError):
         _map_type(list, True)
 
+# ADAPT TO ASYNC TESTS
+# @pytest.mark.unit
+# def test_populate_schema():
+#     schema = {}
 
-def test_populate_schema():
-    class MockDataModel(BaseModel):
-        foo: str
-        bar: Optional[int] = None
+#     foo = MockDataModel.model_fields["foo"]
+#     bar = MockDataModel.model_fields["bar"]
 
-    schema = {}
+#     _populate_schema(field_info=foo, schema=schema)
+#     _populate_schema(field_info=bar, schema=schema)
 
-    foo = MockDataModel.model_fields["foo"]
-    bar = MockDataModel.model_fields["bar"]
+#     assert schema == {"foo": "!string", "bar": "int64"}
 
-    _populate_schema(attr=foo, schema=schema)
-    _populate_schema(attr=bar, schema=schema)
+# ADAPT TO ASYNC TESTS
+# @pytest.mark.unit
+# def test_create_table_schema():
+#     db_connector = DBConnector(
+#         db_name="Test",
+#         username="root",
+#         password="root",
+#         dbtype="mysql",
+#         host="localhost",
+#         port=3306,
+#     )
 
-    assert schema == {"foo": "!string", "bar": "int64"}
+#     data_model = MockDataModel
+#     table_name = "table_name"
+#     schemes = []
 
+#     result = _create_table_schema(
+#         db_connector=db_connector,
+#         data_model=data_model, # type: ignore
+#         table_name=table_name,
+#     )
 
-def test_handle_foreign_keys():
-    db_connector = DBConnector(
-        db_name="Test",
-        username="root",
-        password="root",
-        dbtype="mysql",
-        host="localhost",
-        port=3306,
-    )
+#     pk_command = result.pop("pk_command")
+#     expected_pk = partial(
+#         db_connector._commands.add_primary_key, # type: ignore
+#         table_name=table_name,
+#         primary_key=f"{table_name}_id",
+#         dbconnector=db_connector,
+#     )
 
-    parent = "parent"
-    table_name = "table_name"
-    fk_commands = []
+#     expected_schema = {
+#         "name": "table_name",
+#         "schema": {"foo": "!string", "bar": "int64"},
+#         "fk_commands": [],
+#         "is_primitive": False,
+#         "obj_name": "MockDataModel",
+#     }
 
-    _handle_foreign_keys(
-        parent=parent,
-        table_name=table_name,
-        db_connector=db_connector,
-        fk_commands=fk_commands,
-    )
-
-    expected_kwargs = {
-        "table_name": table_name,
-        "foreign_key": f"{parent}_id",
-        "reference_table": "parent",
-        "reference_column": f"{parent}_id",
-        "dbconnector": db_connector,
-    }
-
-    assert fk_commands[0].keywords == expected_kwargs
-    assert fk_commands[0].func == db_connector._commands.add_foreign_key
-
-
-def test_create_table_schema():
-    db_connector = DBConnector(
-        db_name="Test",
-        username="root",
-        password="root",
-        dbtype="mysql",
-        host="localhost",
-        port=3306,
-    )
-
-    data_model = MockDataModel
-    table_name = "table_name"
-    schemes = []
-
-    result = _create_table_schema(
-        db_connector=db_connector,
-        data_model=data_model,
-        table_name=table_name,
-        schemes=schemes,
-    )
-
-    pk_command = result[0].pop("pk_command")
-    expected_pk = partial(
-        db_connector._commands.add_primary_key,
-        table_name=table_name,
-        primary_key=f"{table_name}_id",
-        dbconnector=db_connector,
-    )
-
-    expected_schema = {
-        "name": "table_name",
-        "schema": {"foo": "!string", "bar": "int64"},
-        "fk_commands": [],
-        "is_primitive": False,
-        "obj_name": "MockDataModel",
-    }
-
-    assert result == [expected_schema]
-    assert pk_command.keywords == expected_pk.keywords
-    assert pk_command.func == expected_pk.func
+#     assert result == expected_schema
+#     assert pk_command.keywords == expected_pk.keywords
+#     assert pk_command.func == expected_pk.func
